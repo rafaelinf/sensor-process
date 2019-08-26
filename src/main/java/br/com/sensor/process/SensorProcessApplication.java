12d67bcd8d65f686c56d02d74485563978d4a731
rafaelsoares <rafael.soares@t2software.com.br>
2019-08-26 03:38:00 -0300

    first commit

---
+++ src/main/java/br/com/sensor/process/SensorProcessApplication.java
@@ -0,0 +1,13 @@
+package br.com.sensor.process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorProcessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sensor/process/SensorProcessApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.sensor.process;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SensorProcessApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
